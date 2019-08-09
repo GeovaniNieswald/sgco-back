1eae3bcd5f000d54e23c9ef87387b6853ec2cabe
Geovani Alex Nieswald <geovaninieswald@gmail.com>
2019-08-08 21:19:08 -0300

    Adicionando projeto Spring

---
+++ sgco/src/main/java/com/dgw/sgco/SgcoApplication.java
@@ -0,0 +1,13 @@
+package com.dgw.sgco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgcoApplication.class, args);
+	}
+
+}
---
+++ sgco/src/test/java/com/dgw/sgco/SgcoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgw.sgco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
