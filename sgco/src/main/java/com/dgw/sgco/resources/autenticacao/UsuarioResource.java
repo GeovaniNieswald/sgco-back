8da084350e2951f0508dacbb1106b663faca011a
Geovani Alex Nieswald <geovaninieswald@gmail.com>
2019-11-20 19:53:42 -0300

    Download de imagem de perfil

--- sgco/src/main/java/com/dgw/sgco/resources/autenticacao/UsuarioResource.java
+++ sgco/src/main/java/com/dgw/sgco/resources/autenticacao/UsuarioResource.java
@@ -1,5 +1,9 @@
 package com.dgw.sgco.resources.autenticacao;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
 import com.dgw.sgco.domain.autenticacao.Usuario;
 import com.dgw.sgco.domain.enums.Permissao;
 import com.dgw.sgco.resources.responses.UploadFileResponse;
@@ -9,6 +13,9 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,6 +48,29 @@ public class UsuarioResource {
                 .toUriString();
 
         return new UploadFileResponse(fileName, fileDownloadUri, file.getContentType(), file.getSize());
+    }
+
+    @RequestMapping(value = "/imagem/download/{fileName:.+}", method = RequestMethod.GET)
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
+        // Load file as Resource
+        Resource resource = fileStorageService.loadFileAsResource(fileName);
+
+        // Try to determine file's content type
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException ex) {
+        }
+
+        // Fallback to the default content type if type could not be determined
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 
     /**
